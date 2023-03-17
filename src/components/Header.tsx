b0b59a866d87a9331bc5e1ab773e3bbfa14bcf6a
Brandon <brandon.cebi.pro@gmail.com>
2023-03-17 10:12:50 +0100

    finish header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,15 @@
+import Logo from "../assets/logo.svg";
 export default function Header() {
-  return <div className="bg-pink-300">Header</div>;
+  return (
+    <div className="py-8">
+      <div className="container mx-auto">
+        <div className="flex justify-between items-center">
+          <a href="#">
+            <img src={Logo} alt="Logo Brandon CEBI" />
+          </a>
+          <button className="btn btn-sm">Work with me</button>
+        </div>
+      </div>
+    </div>
+  );
 }
